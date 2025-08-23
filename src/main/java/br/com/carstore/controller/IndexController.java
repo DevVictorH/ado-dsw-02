2764bb853d2d8b5f90bd0f15e89cbcef1c7195fd
victor hugo <victorvilto@gmail.com>
2025-08-23 18:38:56 -0300

    feat: retornar json na requisção post

--- src/main/java/br/com/carstore/controller/IndexController.java
+++ src/main/java/br/com/carstore/controller/IndexController.java
@@ -2,8 +2,7 @@ package br.com.carstore.controller;
 
 import br.com.carstore.model.Car;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class IndexController {
@@ -11,17 +10,19 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "<h2>Create Car</h2>" +
-                "<label>Car Name</label>" +
-                "<input type=\"text\" name=\"car-name\" id=\"car-name\">" +
-                "<button type=\"submit\">Register</button>";
+                "<form action= '/car' method='post'>" +
+                "<label>Car Name</label> <br>" +
+                "<input type='text' name='name' id='car-name'>" +
+                "<br> <br>"+
+                "<label>Car Color</label> <br>" +
+                "<input type='text' name='color' id='car-color'>" +
+                "<br> <br>"+
+                "<button type='submit'>Register</button>" +
+                "</form>";
     }
 
-    @GetMapping("/car")
-    public ResponseEntity<Car> home(){
-        Car car = new Car();
-        car.setName("nivus");
-        car.setColor("preto");
-
+    @PostMapping("/car")
+    public ResponseEntity<Car> home(@ModelAttribute Car car){
         return ResponseEntity.ok(car);
     }
 
