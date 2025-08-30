7f4cc1f10ea33c6aa2646e2a76167344c23d9198
victor hugo <victorvilto@gmail.com>
2025-08-30 16:11:18 -0300

    Fix: refatorando o codigo

--- src/main/java/br/com/carstore/controller/IndexController.java
+++ src/main/java/br/com/carstore/controller/IndexController.java
@@ -7,20 +7,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
-        return "<h2>Create Car</h2>" +
-                "<form action= '/car' method='post'>" +
-                "<label>Car Name</label> <br>" +
-                "<input type='text' name='name' id='car-name'>" +
-                "<br> <br>"+
-                "<label>Car Color</label> <br>" +
-                "<input type='text' name='color' id='car-color'>" +
-                "<br> <br>"+
-                "<button type='submit'>Register</button>" +
-                "</form>";
-    }
-
     @PostMapping("/car")
     public ResponseEntity<Car> home(@ModelAttribute Car car){
         return ResponseEntity.ok(car);
